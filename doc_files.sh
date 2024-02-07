4778130b4fdb9cc9b288ccea9670b8c46dbf9a60
nikeokoronkwo <150845642+nikeokoronkwo@users.noreply.github.com>
2024-02-07 00:38:38 -0500

    Removed doc_files.sh

--- doc_files.sh
+++
@@ -1,8 +0,0 @@
-foldernames=( "pheasant_temp" "pheasant" "pheasant_assets" "pheasant_cli" "pheasant_meta" "pheasant_build" )
-
-for ELEMENT in ${foldernames[@]}
-do
-    cd "${ELEMENT}"
-    dart doc
-    cd ..
-done
