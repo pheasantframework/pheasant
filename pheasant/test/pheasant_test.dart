ca088cedb81d4289dc6e0361273875b51483c702
Nike <nikechukwu@gmail.com>
2024-02-15 16:12:57 -0500

    Pheasant Code Formatting

--- pheasant/test/pheasant_test.dart
+++ pheasant/test/pheasant_test.dart
@@ -1,16 +1,11 @@
-
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-
-
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-
-    });
+    test('First Test', () {});
   });
 }
