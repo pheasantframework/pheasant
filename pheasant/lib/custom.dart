33b638c38d2c247c36af8609497f789026431882
Nike <nikechukwu@gmail.com>
2024-03-04 16:08:31 -0500

    Added Custom Interface

---
+++ pheasant/lib/custom.dart
@@ -0,0 +1,5 @@
+library custom;
+
+export 'package:pheasant_temp/pheasant_custom.dart' show PheasantComponent, html;
+export 'dart:html';
+export 'package:pheasant_temp/pheasant_build.dart' show TemplateState, State;
