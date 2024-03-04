bf845c55fbe45b4a6b6c2545dfa35239da6f9341
Nike <nikechukwu@gmail.com>
2024-03-04 16:11:07 -0500

    Formatting

--- pheasant/lib/custom.dart
+++ pheasant/lib/custom.dart
@@ -1,5 +1,6 @@
 library custom;
 
-export 'package:pheasant_temp/pheasant_custom.dart' show PheasantComponent, html;
+export 'package:pheasant_temp/pheasant_custom.dart'
+    show PheasantComponent, html;
 export 'dart:html';
 export 'package:pheasant_temp/pheasant_build.dart' show TemplateState, State;
