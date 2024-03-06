845675701462c0cb48af0ab08147957ae44330c0
Nike <nikechukwu@gmail.com>
2024-03-06 13:04:25 -0500

    Added Preamble

--- pheasant_cli/lib/src/utils/enum/platforms.dart
+++ pheasant_cli/lib/src/utils/enum/platforms.dart
@@ -1,1 +1,9 @@
+// Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
+// Please see the AUTHORS files for more information.
+// Intellectual property of third-party.
+// 
+// This file, as well as use of the code in it, is governed by an MIT License
+// that can be found in the LICENSE file.
+// You may not use this file except in compliance with the License.
+  
 
