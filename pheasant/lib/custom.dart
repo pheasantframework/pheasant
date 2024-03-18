aadebc043d5611844a7211cd77061386da525ba5
Nike <nikechukwu@gmail.com>
2024-03-18 11:28:13 -0400

    Added New Custom Components API

--- pheasant/lib/custom.dart
+++ pheasant/lib/custom.dart
@@ -4,3 +4,4 @@ export 'package:pheasant_temp/pheasant_custom.dart'
     show PheasantComponent, html;
 export 'dart:html';
 export 'package:pheasant_temp/pheasant_build.dart' show TemplateState, State;
+export 'package:pheasant_build/pheasant_build.dart' show PheasantApp, PheasantBaseApp;
