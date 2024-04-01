804bd7be7bef5ad03c19c60bbc28a6f6552d8b37
Nike <nikechukwu@gmail.com>
2024-04-01 08:30:12 -0400

    Redid Preamble

--- pheasant/lib/build.dart
+++ pheasant/lib/build.dart
@@ -1,10 +1,11 @@
 // Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
 // Please see the AUTHORS files for more information.
 // Intellectual property of third-party.
-//
+// 
 // This file, as well as use of the code in it, is governed by an MIT License
-// that can be found in the LICENSE file.
-// You may not use this file except in compliance with the License.
+// that can be found in the LICENSE file. 
+// You may use this file only in accordance with the license.
+// https://mit-license.org
 
 /// This is the library used in all generated pheasant files, from the compiled dart code generated from the Pheasant Components to the `main.phs.dart` file also.
 ///
--- pheasant/lib/custom.dart
+++ pheasant/lib/custom.dart
@@ -1,10 +1,11 @@
 // Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
 // Please see the AUTHORS files for more information.
 // Intellectual property of third-party.
-//
+// 
 // This file, as well as use of the code in it, is governed by an MIT License
-// that can be found in the LICENSE file.
-// You may not use this file except in compliance with the License.
+// that can be found in the LICENSE file. 
+// You may use this file only in accordance with the license.
+// https://mit-license.org
 
 /// The Pheasant Framework Plugins/Custom Components API
 ///
